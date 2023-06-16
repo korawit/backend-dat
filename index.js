327551586605ef1194ef9b188869612233454aaa
korawit <korawit.orkphol@gmail.com>
2023-06-16 13:57:29 +0000

    First version backend

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE",
+    );
+    next();
+});
+app.use(express.json());
+const products=[
+    {id:0,name:"Notebook Acer Swift",price:45900,img:"https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg"},
+    {id:1,name:"Notebook Asus Vivo",price:19900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg"},
+    {id:2,name:"Notebook Lenovo Ideapad",price:32900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg"},
+    {id:3,name:"Notebook MSI Prestige",price:54900,img:"https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg"},
+    {id:4,name:"Notebook DELL XPS",price:99900,img:"https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg"},
+    {id:5,name:"Notebook HP Envy",price:46900,img:"https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg"},
+    {id:6,name:"cat",price:46900,img:"https://http.cat/images/500.jpg"}];
+app.get('/api/products',function(req,res){
+    if(products.length>0){
+        
+        res.send(products);
+    }
+    else 
+        res.status(400).send("No product founds");
+});
+app.get('/api/products/:id',function(req,res){
+    const id=req.params.id;
+    const product = products.find(item=>item.id==id);
+    if(product) res.send(product);
+    else res.status(400).send('Not found product for'+id);
+});
+const port = process.env.port || 3001;
+app.listen(port,function(){
+    console.log('Listening on port',port);
 })
